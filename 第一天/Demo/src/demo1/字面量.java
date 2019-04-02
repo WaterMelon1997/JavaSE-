e46be22bb3db17ea917be8e52412f34b29c86683
WaterMelon1997 <3319752068@qq.com>
2019-04-02 14:19:08 +0800

    第一天的Java学习

---
+++ 第一天/Demo/src/demo1/字面量.java
@@ -0,0 +1,31 @@
+package demo1;
+
+public class 字面量 {
+	public static void main(String[] args) {
+//		整型字面量
+		
+		int num1 = 0B010101;   // 2进制
+		int num2 = 012345;     // 8进制
+		int num = 10;          //10进制
+		int num3 = 0XFF;       //16进制
+//		在输出时，都会自动转换为十进制数字
+		
+		long num4 = 456L;
+		
+		float num5 = 2.31F;
+		double num6 = 2.3;
+		
+		boolean num7 = true;
+		num7 = false;
+		
+		char num8 = '5';
+		
+		
+		String num9 =  "字符串";
+		
+		System.out.println(num9);
+		
+		
+				
+	}
+}
